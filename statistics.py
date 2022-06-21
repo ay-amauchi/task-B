695250853faadc456789481b64d64df9fc0d11e7
ay-amauchi <shanneicai0124@gmail.com>
2022-06-21 22:42:28 +0900

    update

--- statistics.py
+++ statistics.py
@@ -1,5 +1,7 @@
+# リスト
 numbers = input(("データを入力してください(スペース区切り) > ")).split()
 
+# 合計値
 sum_number = 0
 for r in range(0, len(numbers)):
     number = int(numbers[r])
@@ -7,13 +9,23 @@ for r in range(0, len(numbers)):
 
 print(f"合計値: {sum_number}")
 
+# 最大値
+max_number = 0
 for r in range(0, len(numbers)):
-    max_number = int(numbers[r])
-    if max_number 
-    
+    num = int(numbers[r])
+    if num > max_number:
+        max_number = num
 
-print(f"最大値: {max(numbers)}")
-print(f"最小値: {min(numbers)}")
+print(f"最大値: {max_number}")
 
+# 最小値
+min = 0
+for r in range(0, len(numbers)):
+    if numbers[min] > numbers[r]:
+        min = r
+
+print(f"最小値: {numbers[min]}")
+
+# 平均値
 average = sum_number / len(numbers)
 print(f"平均値: {average}")
